9c6cb41d146ee53bc6972eb7db4b95b591ed4239
Shravan250 <shravanbobade25@gmail.com>
2025-07-27 15:06:15 +0530

    fixed the routing problem

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,24 +3,32 @@ import { dummyInterviews } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import InterviewCard from "@/components/InterviewCard"
+import { InterviewCard } from "@/components/InterviewCard";
 
 const page = () => {
   return (
     <>
-    <br />
+      <br />
       <section className="card-cta">
         <div className="flex flex-col gap-6 max-w-lg">
           <h2>Get Interview Ready with AI-Powered Practice & Feedback</h2>
-          
-          <p className="text-lg"> Practice on rael Interview questions &  get instant feedback </p>
+
+          <p className="text-lg">
+            {" "}
+            Practice on rael Interview questions & get instant feedback{" "}
+          </p>
 
           <Button asChild className="btn-primary max-sm:w-full">
             <Link href="/interview">Start an Interview</Link>
           </Button>
-
-        </div> 
-        <Image src="/robot.png" alt="robots" width={400} height={400} className="max-sm:hidden"></Image>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robots"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        ></Image>
       </section>
 
       <section className="flex flex-col gap-6 mt-8">
@@ -28,24 +36,24 @@ const page = () => {
 
         <div className="interviews-section">
           {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/> 
-          ))} 
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
         </div>
       </section>
-    
+
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
 
         <div className="interviews-section">
           {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview} key={interview.id}/> 
-          ))} 
+            <InterviewCard {...interview} key={interview.id} />
+          ))}
 
           {/* <p>You haven&apos;t taken any interviews</p> */}
         </div>
       </section>
     </>
-  )
+  );
 };
 
 export default page;
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import dayjs from 'dayjs';
-import Image from 'next/image';
-import Link from 'next/link';
-import { getRandomInterviewCover } from '@/lib/utils';
-import { Button } from './ui/button';
-import DisplayTechIcons from './DisplayTechIcons';
+import React from "react";
+import dayjs from "dayjs";
+import Image from "next/image";
+import Link from "next/link";
+import { getRandomInterviewCover } from "@/lib/utils";
+import { Button } from "./ui/button";
+import DisplayTechIcons from "./DisplayTechIcons";
 
 interface InterviewCardProps {
-  interviewId: string;
+  id: string;
   userId: string;
   role: string;
   type: string;
@@ -15,10 +15,19 @@ interface InterviewCardProps {
   createdAt: string;
 }
 
-const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+export const InterviewCard = ({
+  id,
+  userId,
+  role,
+  type,
+  techstack,
+  createdAt,
+}: InterviewCardProps) => {
   const feedback = null as Feedback | null;
-  const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
-  const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY');
+  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+  const formattedDate = dayjs(
+    feedback?.createdAt || createdAt || Date.now()
+  ).format("MMM D, YYYY");
 
   return (
     <div className="card-border w-[360px] max-sm:w-full min-h-96">
@@ -45,28 +54,38 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }
           <div className="flex flex-row gap-5 mt-3 items-center">
             {/* Date */}
             <div className="flex flex-row gap-2 items-center">
-              <Image src="/calendar.svg" alt="calendar" width={22} height={22} />
+              <Image
+                src="/calendar.svg"
+                alt="calendar"
+                width={22}
+                height={22}
+              />
               <p>{formattedDate}</p>
             </div>
 
             {/* Score */}
             <div className="flex flex-row gap-2 items-center">
               <Image src="/star.svg" alt="star" width={22} height={22} />
-              <p>{feedback?.totalScore ?? '---'}/100</p>
+              <p>{feedback?.totalScore ?? "---"}/100</p>
             </div>
           </div>
 
           {/* Assessment */}
           <p className="line-clamp-2 mt-5">
-            {feedback?.finalAssessment || "You haven't taken the interview yet. Take it now to improve your skills."}
+            {feedback?.finalAssessment ||
+              "You haven't taken the interview yet. Take it now to improve your skills."}
           </p>
 
           {/* Footer */}
           <div className="flex flex-row justify-between mt-5 items-center">
-            <DisplayTechIcons techStack={techstack}/>
+            <DisplayTechIcons techStack={techstack} />
             <Button className="btn-primary">
-              <Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>
-                {feedback ? 'Check Feedback' : 'View Interview'}
+              <Link
+                href={
+                  feedback ? `/interview/${id}/feedback` : `/interview/${id}`
+                }
+              >
+                {feedback ? "Check Feedback" : "View Interview"}
               </Link>
             </Button>
           </div>
