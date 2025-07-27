1a08c0dd4a6c0629568b65ba68716a0ef4c5d994
Shravan250 <shravanbobade25@gmail.com>
2025-07-27 15:14:23 +0530

    fixed peer dependancy error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,28 +1,27 @@
-import { isAuthenticated } from "@/lib/actions/auth.action";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
-
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
-
   //to check valid user authentication
   const isUserAuthenticated = await isAuthenticated();
 
-  if (!isUserAuthenticated) redirect('/sign-in'); 
-   
+  if (!isUserAuthenticated) redirect("/sign-in");
 
-  return <div className="root-layout">
-    <nav>
-      <Link href="/" className="flex items-center gap-2"> 
-        <Image src="/logo.svg" alt="logo" height={32} width={38} />
-        <h2 className="text-primary-100">HireIQ</h2>
-      </Link>
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" height={32} width={38} />
+          <h2 className="text-primary-100">HireIQ</h2>
+        </Link>
 
-      {children}
-    </nav>
-  </div>;
+        {children}
+      </nav>
+    </div>
+  );
 };
 
 export default RootLayout;
